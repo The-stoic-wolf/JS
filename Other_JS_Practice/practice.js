81d6e0d0748a8f89188300cb46e4fb46d39499fe
the stoic <1219muhammadishaq@gmail.com>
2025-06-08 19:26:10 +0500

    update other practice file june 8,2025 sunday.

--- Other_JS_Practice/practice.js
+++ Other_JS_Practice/practice.js
@@ -1,12 +1,3 @@
-const user1 = {
-  name: "James",
-  class: 4,
-  greet: function() {
-    const now = () => {
-      let userName = "James";
-      console.log(`This is user_1: ${this.name}`)
-    }
-    now();
-  } 
-}
-user1.greet()
+setTimeout(() => {
+console.log("Delayed Hello");
+}, 1000);
