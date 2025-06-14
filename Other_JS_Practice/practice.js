ff576bcc8e0464a613691310a9a1fc51d8006862
stoic <muhammadishaq@gmail.com>
2025-06-14 14:34:13 -0700

    add other folder

--- Other_JS_Practice/practice.js
+++ Other_JS_Practice/practice.js
@@ -1,3 +1,14 @@
-setTimeout(() => {
-console.log("Delayed Hello");
-}, 1000);
+function doSomethingAsync(callback) {
+  setTimeout(function(){
+    console.log("Done!");
+    callback();
+  }, 1000);
+}
+
+doSomethingAsync(function(){
+  console.log("Callback called.");
+});
+
+// Prints:
+// Done!
+// Callback called.
